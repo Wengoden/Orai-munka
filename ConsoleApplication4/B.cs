535b9186a44706a63e8adec8ea9dfe664fa27054
MartonLabancz <labancz.marton@gmail.com>
2017-02-23 12:31:46 +0100

    Add project files.

---
+++ ConsoleApplication4/B.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication4
+{
+    public class B : A
+    {
+        protected void S4()
+        {
+            Console.WriteLine("B::V1");
+            V1();
+        }
+
+        public void S5()
+        {
+            Console.WriteLine("B::S3");
+            S3();
+        }
+
+        protected void S3()
+        {
+            Console.WriteLine("B::S3");
+        }
+
+        public void V2()
+        {
+            Console.WriteLine("B::V2");
+            V1();
+        }
+
+        private void V1()
+        {
+            Console.WriteLine("B::V1");
+            S5();
+        }
+    }
+}
---
+++ ConsoleApplication4/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication4
+{
+    public class A
+    {
+        public void S1(string[] args)
+        {
+            Console.WriteLine("A::S1");
+            V1();
+        }
+
+        protected void V1()
+        {
+            Console.WriteLine("A::V1");
+            S3();
+        }
+
+        public void S2()
+        {
+            Console.WriteLine("A::S2");
+            S1();
+        }
+
+        public void S3()
+        {
+            Console.WriteLine("A::S3");
+        }
+    }
+}
